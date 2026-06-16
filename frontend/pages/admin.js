d4f3d543b39db7543a1afcecc292dc5495483914
newrduenas14 <newrduenas14@gmail.com>
2026-06-16 15:22:47 -0500

    Use cached API on admin

--- frontend/pages/admin.js
+++ frontend/pages/admin.js
@@ -1,4 +1,4 @@
-import { listLocations, resetToSpreadsheetSeed } from "../js/api.js?v=phonefix1";
+import { listLocations, resetToSpreadsheetSeed } from "../js/api-smooth1.js";
 import { notice, table } from "../js/utils.js";
 
 export async function render(ctx) {
