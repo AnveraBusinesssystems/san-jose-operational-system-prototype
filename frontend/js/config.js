0defd33253682c82a8e591fdf219d4e190465188
Anvera_Business_Systems <main@anverabusinesssytems.com>
2026-07-10 15:32:46 -0500

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,1 +1,1 @@
-export const GOOGLE_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbycHxNmFS1gh3_xD5bXXuBvEeUr12zJcpTrBBxGCrDMnxdmeWjEMW9dPXszMg95CpxR/exec";
+export const GOOGLE_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyrzcflWFwowVrctvL-js5zMf-OXobDNYR4NIads_04LNdbMCNdI7s6Nn7lF24-Hi88/exec";
