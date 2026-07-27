a39dc37a9972aae78a14da2720ce430ab4a7cba4
Anvera_Business_Systems <main@anverabusinesssytems.com>
2026-07-27 14:38:46 -0500

    Update GOOGLE_SCRIPT_WEB_APP_URL to new endpoint

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,1 +1,1 @@
-export const GOOGLE_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbypGrpIvFUpS6c57Af-zC4OiK_dFktYYQlwtyXv3QvB4Z9BRPEqz05tI4B4KwDxQAtf/exec";
+export const GOOGLE_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxHvPl64-UJEVQrFOVniAgPSYnz8QMNwmeuj-0iZQVKw25y-zLV_KvK_kO8S6LuxKSW/exec";
